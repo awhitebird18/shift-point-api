820b243ad2b7d2650aede3f97e2ced8631d467f2
awhitebird19 <aaronwhitebirdsnt@gmail.com>
2022-07-14 14:11:09 -0400

    5th upload

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
+
+app.use(cors());
 
 app.use("/timesheetrules", timesheetRulesRoutes);
 app.use("/timesheet", timesheetRoutes);
