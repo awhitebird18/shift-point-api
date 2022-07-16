024184fd971a8042267b03ca03ba2a113e5dfed2
awhitebird19 <aaronwhitebirdsnt@gmail.com>
2022-07-16 14:39:21 -0400

    remove cors

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
 
 // app.use(cors());
 
