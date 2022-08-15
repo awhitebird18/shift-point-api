26973a8fc766e0c1939235b6186ebc5fe364fde1
awhitebird18 <aaron.whitebird@gmail.com>
2022-08-14 22:24:28 -0400

    Adjusted Cors

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://shift-point.web.app",
   })
 );
 // app.use(
